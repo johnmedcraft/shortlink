f630405f7be1f594380476ff8f0aaaef61f520a4
johnmedcraft <jmedcraft@appriss.com>
2018-02-20 21:51:53 +0000

    first commit of this project in progress

---
+++ client/main.js
@@ -0,0 +1,16 @@
+
+import {Meteor} from 'meteor/meteor';
+import ReactDOM from 'react-dom';
+import {Tracker} from 'meteor/tracker';
+
+import {routes, onAuthChange} from '../imports/routes/routes';
+
+Tracker.autorun(() => {
+  const isAuthenticated = !!Meteor.userId(); 
+  onAuthChange(isAuthenticated);
+  console.log('isAuthenticated', isAuthenticated);
+});
+
+Meteor.startup(() => {
+  ReactDOM.render(routes, document.getElementById('app'));
+});
---
+++ imports/ui/Login.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link } from 'react-router';
+import { Meteor } from 'meteor/meteor';
+
+export default class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: ''
+    };
+  }
+  onSubmit(e) {
+    e.preventDefault();
+    let email = this.refs.email.value.trim();
+    let password = this.refs.password.value.trim();
+    
+    Meteor.loginWithPassword({email}, password, (err) => {
+      if (err) {
+        this.setState({error: err.reason});
+      } else {
+        this.setState({error: ''});
+      }
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h1>Short Lnk</h1>
+        {this.state.error ? <p>{this.state.error}</p> : undefined}
+        <form onSubmit={this.onSubmit.bind(this)} noValidate>
+          <input type="email" ref="email" name="email" placeholder="email" />
+          <input type="password" ref="password" name="password" placeholder="password" />
+          <button>Login</button>
+        </form>
+
+        <Link to="/signup">Create an Account</Link>
+      </div>
+    );
+  }
+}
